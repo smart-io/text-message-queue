0abcfc20b7ec43de2b2b085ed12b1c6ca6a9f93a
Kevin <kevin@picksmart.io>
2015-04-22 12:03:27 -0400

    Implemented twillio api

--- src/Driver/Twillio.php
+++ src/Driver/Twillio.php
@@ -3,6 +3,7 @@
 namespace Smart\TextMessageQueue\Driver;
 
 use Exception;
+use Services_Twilio;
 use Smart\TextMessageQueue\TextMessageQueueEntity;
 
 class Twillio implements DriverInterface
@@ -10,14 +11,29 @@ class Twillio implements DriverInterface
     /**
      * @var string
      */
-    private $apiKey;
+    private $accountId;
 
     /**
-     * @param $apiKey
+     * @var string
      */
-    public function __construct($apiKey)
+    private $authToken;
+
+    /**
+     * @var string
+     */
+    private $fromNumber;
+
+    /**
+     * @param string $acountId
+     * @param string $authToken
+     * @param string $fromNumber
+     */
+    public function __construct($acountId, $authToken, $fromNumber)
     {
-        $this->setApiKey($apiKey);
+
+        $this->setAccountId($acountId);
+        $this->setAuthToken($authToken);
+        $this->setFromNumber($fromNumber);
     }
 
     /**
@@ -29,26 +45,77 @@ class Twillio implements DriverInterface
     public function send(TextMessageQueueEntity $textMessageQueue)
     {
 
-        //todo : implement twillio
-        return false;
+        $client = new Services_Twilio($this->getAccountId(),
+            $this->getAuthToken());
+
+        foreach ($textMessageQueue->getTo() as $recipientNumber) {
+
+            $message = $client->account->messages->create(array(
+                "From" => $this->getFromNumber(),
+                "To" => $recipientNumber,
+                "Body" => $textMessageQueue->getBody(),
+            ));
+        }
+
+        return !empty($message) && !empty($message->sid);
     }
 
     /**
      * @return string
      */
-    public function getApiKey()
+    public function getAccountId()
     {
-        return $this->apiKey;
+        return $this->accountId;
     }
 
     /**
-     * @param string $apiKey
+     * @param string $accountId
      *
      * @return $this
      */
-    public function setApiKey($apiKey)
+    public function setAccountId($accountId)
     {
-        $this->apiKey = $apiKey;
+        $this->accountId = $accountId;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthToken()
+    {
+        return $this->authToken;
+    }
+
+    /**
+     * @param string $authToken
+     *
+     * @return $this
+     */
+    public function setAuthToken($authToken)
+    {
+        $this->authToken = $authToken;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFromNumber()
+    {
+        return $this->fromNumber;
+    }
+
+    /**
+     * @param string $fromNumber
+     *
+     * @return $this
+     */
+    public function setFromNumber($fromNumber)
+    {
+        $this->fromNumber = $fromNumber;
 
         return $this;
     }
